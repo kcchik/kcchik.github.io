32232f272792a51561dc1397f48e58bb7d920283
kcchik <kcchik@edu.uwaterloo.ca>
2018-02-15 11:44:57 -0500

    Commit

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 $(function() {
-  $(".header").slideDown("slow");
+
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll > 350)
+      $(".name").hide();
+    else
+      $(".name").show();
+    if (scroll > 233)
+      $(".headerlink").hide();
+    else
+      $(".headerlink").show();
+  });
+
+  $(".name").animate({
+    opacity: 1,
+  }, 1500);
+
+  $(".headerlink").animate({
+    opacity: 1,
+  }, 1500);
+
   $(".col-md-4").on({
     mouseenter: function() {
       $(this).find(".logo").animate({
@@ -14,4 +34,5 @@ $(function() {
       $(this).find(".worktitle").css("color", "#2d3b46");
     }
   });
+
 });
