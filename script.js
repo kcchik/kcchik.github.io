a0e2c42efb9a681cee87342a9b3d8ca1e2e24353
kcchik <kcchik@edu.uwaterloo.ca>
2018-02-15 22:11:06 -0500

    Commit

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 $(function() {
 
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    $(".header").css("background-position", "center -" + (scroll*0.5) + "px");
+  });
+
   $(".name").animate({
     opacity: 1,
   }, 1500);
@@ -8,18 +13,78 @@ $(function() {
     opacity: 1,
   }, 1500);
 
-  $(".col-md-4").on({
+  $(".work-0").on({
     mouseenter: function() {
-      $(this).find(".logo").animate({
-        height: "+=10px"
-      }, "fast");
-      $(this).find(".worktitle").css("color", "#339baf");
+      $(this).find(".work-logo").animate({
+        opacity: "0"
+      }, "fast", function() {
+        $(".work-0").find(".work-logo").hide();
+        $(".work-0").find(".work-description").show();
+        $(".work-0").find(".work-description").animate({
+          opacity: "1"
+        });
+      });
     },
     mouseleave: function() {
-      $(this).find(".logo").animate({
-        height: "-=10px"
-      }, "fast");
-      $(this).find(".worktitle").css("color", "#2d3b46");
+      $(this).find(".work-description").animate({
+        opacity: "0"
+      }, "fast", function() {
+        $(".work-0").find(".work-description").hide();
+        $(".work-0").find(".work-logo").show();
+        $(".work-0").find(".work-logo").animate({
+          opacity: "1"
+        }, "fast");
+      });
+    }
+  });
+
+  $(".work-1").on({
+    mouseenter: function() {
+      $(this).find(".work-logo").animate({
+        opacity: "0"
+      }, function() {
+        $(".work-1").find(".work-logo").hide();
+        $(".work-1").find(".work-description").show();
+        $(".work-1").find(".work-description").animate({
+          opacity: "1"
+        });
+      });
+    },
+    mouseleave: function() {
+      $(this).find(".work-description").animate({
+        opacity: "0"
+      }, "fast", function() {
+        $(".work-1").find(".work-description").hide();
+        $(".work-1").find(".work-logo").show();
+        $(".work-1").find(".work-logo").animate({
+          opacity: "1"
+        });
+      });
+    }
+  });
+
+  $(".work-2").on({
+    mouseenter: function() {
+      $(this).find(".work-logo").animate({
+        opacity: "0"
+      }, function() {
+        $(".work-2").find(".work-logo").hide();
+        $(".work-2").find(".work-description").show();
+        $(".work-2").find(".work-description").animate({
+          opacity: "1"
+        });
+      });
+    },
+    mouseleave: function() {
+      $(this).find(".work-description").animate({
+        opacity: "0"
+      }, "fast", function() {
+        $(".work-2").find(".work-description").hide();
+        $(".work-2").find(".work-logo").show();
+        $(".work-2").find(".work-logo").animate({
+          opacity: "1"
+        }, "fast");
+      });
     }
   });
 
