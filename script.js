d3fa854e1942c77fd4614768dad1dd7636bbb44c
kcchik <kcchik@edu.uwaterloo.ca>
2018-02-19 16:37:32 -0500

    Commit

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 $(function() {
 
-  $(window).scroll(function(){
-    var scroll = $(window).scrollTop();
-    $(".header").css("background-position", "center -" + (scroll*0.5) + "px");
-  });
-
-  $(".name").animate({
-    opacity: 1,
+  $(".header-image").animate({
+    opacity: 1
   }, 1500);
 
-  $(".header-link").animate({
-    opacity: 1,
-  }, 1500);
-
-  $(".link").on({
+  $(".work-link").on({
     mouseenter: function() {
-      $(this).find(".work-overlay").css("opacity", "0.2");
+      $(this).find(".work-hover").css("opacity", "0.2");
     },
     mouseleave: function() {
-      $(this).find(".work-overlay").css("opacity", "0");
+      $(this).find(".work-hover").css("opacity", "0");
     }
   });
 
