7416e74bc42e4d02381cb889a274b5c4511b196b
Kevin Chik <kevin.c.chik@gmail.com>
2018-05-29 20:18:34 -0400

    Soft boiled my compter

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
 $(function() {
 
   $(".header").animate({
-    opacity: 1
+    opacity: 1,
   }, 1500);
+
+  $("#resume").on({
+    mouseenter: function() {
+      $(".popup").animate({
+        bottom: "0"
+      }, 150);
+    },
+    mouseleave: function() {
+      $(".popup").animate({
+        bottom: "-250px"
+      }, 150);
+    }
+  });
 
   $(".work-link").on({
     mouseenter: function() {
-      $(this).find(".work-img").css("opacity", "0.7");
+      $(this).find(".work-img").animate({
+        opacity: 0.7
+      }, 150);
     },
     mouseleave: function() {
-      $(this).find(".work-img").css("opacity", "1");
+      $(this).find(".work-img").animate({
+        opacity: 1
+      }, 150);
     }
   });
 
